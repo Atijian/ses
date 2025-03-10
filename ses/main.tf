f57f624ff1b46671c6ec96d67c8757e19274d438
Atijian <legitcydoc@gmail.com>
2025-03-10 17:58:49 +0530

    ju

--- ses/main.tf
+++ ses/main.tf
@@ -1,9 +1,33 @@
+variable "sender" {
+  description = "The email address to send from"
+  type        = string
+}
+
+variable "recipient" {
+  description = "The email address to send to"
+  type        = string
+}
+
+variable "subject" {
+  description = "The subject of the email"
+  type        = string
+}
+
 resource "aws_ses_email_identity" "sender" {
-  email = "legitcydoc@gmail.com"  # Replace with your sender email
+  email = var.sender
 }
 
 resource "aws_ses_email_identity" "recipient" {
-    email = "attacktitan1989@gmail.com"
-  
+  email = var.recipient
 }
 
+resource "null_resource" "send_email" {
+  provisioner "local-exec" {
+    command = <<EOT
+aws ses send-email \
+  --from "${var.sender}" \
+  --destination "ToAddresses=${var.recipient}" \
+  --message "Subject={Data=${var.subject}},Body={Text={Data=Hello, this is a test email sent using Terraform and SES.}}"
+EOT
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
 module "ses" {
   source       = "./ses"
 }
+
+module "ses" {
+  source    = "./ses"
+  sender    = "legitcydoc@gmail.com"  # Replace with your sender email
+  recipient = "attacktitan1989@gmail.com"  # Replace with your recipient email
+  subject   = "Test Email from Terraform"
+}
