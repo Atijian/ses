3c21e14c22ec1258b1038bfac4d105899155d605
Atijian <legitcydoc@gmail.com>
2025-03-11 13:35:38 +0530

    setting up checks

--- .github/workflows/ses/main.tf
+++ .github/workflows/ses/main.tf
@@ -21,13 +21,21 @@ resource "aws_ses_email_identity" "recipient" {
   email = var.recipient
 }
 
+
 resource "null_resource" "send_email" {
   provisioner "local-exec" {
     command = <<EOT
 aws ses send-email \
   --from "${var.sender}" \
   --destination "ToAddresses=${var.recipient}" \
-  --message "Subject={Data=${var.subject}},Body={Text={Data=Hello, this is a test email sent using Terraform and SES.}}"
+  --message "Subject={Data=${var.subject},Charset=utf8},Body={Text={Data=Hello, this is a test email sent using Terraform and SES.,Charset=utf8}}" \
+  --output json > output.json
+
+if [ $? -eq 0 ]; then
+  echo "Email sent successfully!"
+else
+  echo "Failed to send email."
+fi
 EOT
   }
 }
