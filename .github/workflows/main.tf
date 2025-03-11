fbf625a04a6e23bfbeaa0b65376390dac4b5c444
Atijian <legitcydoc@gmail.com>
2025-03-11 13:18:37 +0530

    f

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -2,6 +2,17 @@ terraform {
   required_version = "~> 1.5.0"
 }
 
+provider "aws" {
+  region      = var.aws_region
+  max_retries = 10
+
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  token      = var.aws_token
+
+
+}
+
 module "ses" {
   source    = "./ses"
   sender    = "legitcydoc@gmail.com"  # Replace with your sender email
